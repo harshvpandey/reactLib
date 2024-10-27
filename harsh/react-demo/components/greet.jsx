6236ee55150646a3dca711997d57e11f52e43d1b
harshvpandey <hvpandey2000@gmail.com>
2024-10-28 01:08:18 +0530

    react

---
+++ harsh/react-demo/components/greet.jsx
@@ -0,0 +1,6 @@
+function Greet(){
+    return <h1>Hello From Components</h1>
+}
+
+
+export default Greet;
---
+++ harsh/react-demo/src/App.jsx
@@ -0,0 +1,13 @@
+import Greet from "../components/greet";
+import Add from "../components/add";
+function App(){
+  return (
+    <div>
+    <Greet></Greet>
+    <Add></Add>
+    </div>
+    
+  );
+  
+};
+export default App;
