019e71072ff6fd657e495ed2002cb1784ae73685
Harshvardhan Pandey <hvpandey2000@gmail.com>
2024-12-09 20:52:14 +0530

    list visible

--- harsh/to-do-list/src/components/to-do-list.jsx
+++ harsh/to-do-list/src/components/to-do-list.jsx
@@ -7,7 +7,7 @@ const ToDoList=()=>{
     const [newTask, setNewTask] = useState(''); // Input value
     
     const addTask=()=>{
-    newTask=newTask.trim();
+ 
     console.log(tasks)
  
     setTasks([...tasks,{status:'incomplete',task:newTask,taskId:Date.now()}])
@@ -19,7 +19,19 @@ return(
  
         <input placeholder="Add Tasks" className='input-box' value={newTask} onChange={(e) => setNewTask(e.target.value)} ></input>
         <button className='add-button' onClick={() => addTask()}>Add</button>
-       
+        <br></br>
+        <div>
+        {tasks.map((task) => (
+            <div key={task.taskId}>
+              <span>{task.task}</span>
+              <button onClick={() => {
+                setTasks(tasks.map(t => t.taskId === task.taskId ? { ...t, status: 'completed' } : t));
+              }}>
+                Mark as Completed
+              </button>
+            </div>
+          ))}
+        </div>
     </span>
 )
 }
